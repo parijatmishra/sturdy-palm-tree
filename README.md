2f4fa4f2674c9bff208820fc6641195a34c4b9f2
Parijat Mishra <parijat.mishra@gmail.com>
2019-12-07 23:29:17 +0800

    Add another route using macros. Add build and run instructions.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # sturdy-palm-tree
-Rust web service with a web-framework TBD
+Rust web service with the actix-web framework.
+
+## Development
+
+Build:
+
+    $ cargo build
+
+Run (development):
+
+    $ cargo run
+
+Now visit:
+
+    - [http://localhost:8088/]
+    - [http://localhost:8088/hello]
+    - [http://localhost:8088/again]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::get;
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
@@ -8,11 +9,19 @@ fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello, again!")
 }
 
+// alternative
+// using macro attributes to define routes
+#[get("/hello")]
+fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
 fn main() {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
+            .service(index3) // macro defined
     })
     .bind("127.0.0.1:8088")
     .unwrap()
