31751c9c9d6dd9e69262a7bf02a4c34776dc4bbb
Parijat Mishra <parijat.mishra@gmail.com>
2019-12-08 00:04:59 +0800

    App with mutable shared data

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::get;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+
+struct AppData {
+    counter: Mutex<i32>,
+}
 
 fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
 }
 
-fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello, again!")
-}
-
 // alternative
 // using macro attributes to define routes
 #[get("/hello")]
-fn index3() -> impl Responder {
+fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+fn index3(data: web::Data<AppData>) -> impl Responder {
+    let mut counter = data.counter.lock().unwrap(); // get counter's MutexGuard
+    *counter += 1;
+
+    HttpResponse::Ok().body(format!("Hello, again! You've called me {} times", counter))
+}
+
 fn main() {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppData {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+        // move needed because we are using 'counter'
         App::new()
+            .register_data(counter.clone()) // send a clone to the new App instace
             .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
-            .service(index3) // macro defined
+            .service(index2) // alternative - with macro defined "service"
+            .route("/again", web::get().to(index3))
     })
     .bind("127.0.0.1:8088")
     .unwrap()
